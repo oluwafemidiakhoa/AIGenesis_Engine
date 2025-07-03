00916e902c1f2dd45d03a008b59fe6df30e8a57b
oluwafemidiakhoa <38867499+oluwafemidiakhoa@users.noreply.github.com>
2025-07-03 12:24:26 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   content: [
-    './app/templates/**/*.html',
+    "./app/templates/**/*.html",
+    "./app/static/js/**/*.js",
+    "./app/**/*.py"
   ],
   theme: {
-    extend: {
-      colors: {
-        // Define your primary color palette.
-        // You can use any color from the Tailwind CSS color palette.
-        primary: colors.sky,
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
