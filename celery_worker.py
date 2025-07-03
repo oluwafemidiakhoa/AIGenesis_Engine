c96e2dc799abdbc84cd057ee12b35964168722b8
oluwafemidiakhoa <38867499+oluwafemidiakhoa@users.noreply.github.com>
2025-07-03 18:27:24 -0500

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -1,6 +1,8 @@
 # celery_worker.py
 
+import os
 from app import create_app, celery
 
-app = create_app()
+config_name = os.getenv('FLASK_CONFIG') or 'prod'
+app = create_app(config_name)
 app.app_context().push()
