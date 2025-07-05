0626b6ddb2765e82a4c40912869c345c334b9e78
oluwafemidiakhoa <38867499+oluwafemidiakhoa@users.noreply.github.com>
2025-07-04 21:50:10 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,16 @@
+#!/usr/bin/env python
+"""
+WSGI entry point for Gunicorn.
+Run with:
+  gunicorn --bind 0.0.0.0:$PORT wsgi:app
+"""
+
 import os
 from app import create_app
 
-# Create an application instance for the Gunicorn server
-app = create_app(os.getenv("FLASK_CONFIG") or "default")
+# Load the appropriate config: 'dev', 'prod', or 'test'
+config_name = os.getenv('FLASK_CONFIG', 'prod')
+
+# Instantiate the Flask application
+app = create_app(config_name)
+
